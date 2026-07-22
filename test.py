fb62f8b0acab91c02eb37afd257d83ea305ddc4f
Avirup Chakraborty <avirupchakra@gmail.com>
2026-07-22 10:22:20 +0000

    changed api event/run

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ import requests
 
 HOST_PI_IP="10.108.216.54"
 HOST_PI_PORT="5000"
-url = f"http://{HOST_PI_IP}:{HOST_PI_PORT}/api/events/upload"
+url = f"http://{HOST_PI_IP}:{HOST_PI_PORT}/api/events/run"
 LOCATION="FG"
 data_str="""{"Parts document ID":"S107134100","Id":"9IYXdIZ6QZZ8jHOKIyL2","ArrivalLocation":"SA","Type":"FG","Qt":24}"""
 payload = {
