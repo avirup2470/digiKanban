d7458f3b7be1262c8d9dc9f564727e529da38693
Avirup Chakraborty <avirupchakra@gmail.com>
2026-07-22 10:52:03 +0000

    changed payload in test

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ HOST_PI_IP="10.108.216.54"
 HOST_PI_PORT="5000"
 url = f"http://{HOST_PI_IP}:{HOST_PI_PORT}/api/events/run"
 LOCATION="FG"
-data_str="""{"Parts document ID":"S107134100","Id":"9IYXdIZ6QZZ8jHOKIyL2","ArrivalLocation":"SA","Type":"FG","Qt":24}"""
+data_str="""{"Parts document ID": 1, "Id": 101, "ArrivalLocation": "AssemblyLine", "Qt": 10}"""
 payload = {
     "source_location": LOCATION,
     "event_json": data_str
